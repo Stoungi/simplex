a054bde022c3987c603febcc9ff9c1bce024fdd5
Stoungi <36931144+Stoungi@users.noreply.github.com>
2025-05-14 19:43:24 +0000

    Update main.py
    showcase the new and improved syntax!

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 
 from simplex_imp import simplex
 
-c1 = [3, 5]  # c1 = objective function    
 
-A1 = [[1, 0], # A1 = constraints (left handed side)           
-      [0, 2],            
-      [3, 2]]    
+
+"""
+z = 50x + 30y
+
+4x + 3y <= 240
+
+y <= 40
+
+x <= 36
+"""
+
      
-b1 = [4, 12, 18] # b1 = contraints (right handed side)
 
+     
+c = "min -6+7+4"
+Ab = ["2+5-1 <= 18", 
+      "1-1-2 <= -14",
+      "3+2+2 = 26"]
 
-# same idea for the 2nd example here
-c2 = [1, 2]
+c2 = "max 50+30" 
 
-A2 = [[1, 0],
+Ab2 = ["4+3 <= 240",
+      "0+1 <= 40",
+      "1+0 <= 36"] 
       
-      [2, 5]]
+     
 
-b2 = [5, 16]
-
-
-
-simp = [simplex(c1, A1, b1), simplex(c2, A2, b2)]
-
-
-
+simp = [simplex(c, Ab), simplex(c2, Ab2)]
 for a in simp:
-
-    a.show_range()
-    print("Optimal solution:", a.optimal)
-    print("Maximum value:", a.value)
-
-    print("steps taken: ", a.steps)
-
-
+      a.show_steps()
+      print("optimal: ", a.optimal)
+      print("value: ", a.value)
+      print("==================")
 
 
